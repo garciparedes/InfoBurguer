b9705b7f5ce94be3f20d9beabe1c53ea9c6b420f
Sergio García <garciparedes@gmail.com>
2014-12-15 12:10:16 +0100

    Implementada parcialmente clase Combo
    Falta limitar los productos de la clase Combo para que a su vez no
    compongan otros productos que sean Combo.

--- src/Combo.java
+++ src/Combo.java
@@ -14,28 +14,75 @@ public class Combo extends Producto {
     private ArrayList<Producto> productos;
 
 
+    /**
+     * Constructor de la clase Combo.
+     *
+     * @param nombre String con el nombre del Combo.
+     * @param descripcion String con la descripcion del Combo.
+     * @param productos ArrayList de productos  que componen el Combo.
+     */
     public Combo(String nombre, String descripcion, ArrayList<Producto> productos){
         super(nombre, descripcion);
         this.productos = productos;
     }
 
 
+    /**
+     * Setter del atributo productos.
+     *
+     * @param productos Arraylist de productos  que componen el Combo.
+     */
     public void setProductos(ArrayList<Producto> productos) {
         this.productos = productos;
     }
 
 
+    /**
+     * Getter del atributo productos.
+     *
+     * @return productos ArrayLis de productos que componen el Combo.
+     */
     public ArrayList<Producto> getProductos() {
         return productos;
     }
 
+
+    /**
+     * Getter del atributo calorias.
+     *
+     * Para calcular el numero de calorias se suman las de los productos que componen el combo.
+     *
+     * @return totalCalorias Integer con el numero total de calorias del combo.
+     */
     @Override
     public int getCalorias() {
-        return super.getCalorias();
+        int totalCalorias = 0;
+
+        for (int i = 0 ; i < getProductos().size() ; i++){
+            totalCalorias += getProductos().get(i).getCalorias();
+        }
+
+        return totalCalorias;
     }
 
+
+    /**
+     * Getter del atributo precio.
+     *
+     * Para calcular el precio se realiza la suma del de sus productos y a este se le resta el 20%.
+     *
+     * @return totalPrecio Double con el precio total del combo.
+     */
     @Override
     public double getPrecio() {
-        return super.getPrecio();
+        double totalPrecio = 0;
+
+        for (int i = 0 ; i < getProductos().size() ; i++){
+            totalPrecio += getProductos().get(i).getPrecio();
+        }
+
+        totalPrecio = totalPrecio - 0.2*totalPrecio;
+
+        return totalPrecio;
     }
 }
--- src/UsaProductosInfoBurguer.java
+++ src/UsaProductosInfoBurguer.java
@@ -35,6 +35,15 @@ public class UsaProductosInfoBurguer {
         carta1.add(new Hamburguesa("Hamburguesa de pollo","hola",3.9, 2000,"pollo, lechuga, tomate"));
         carta1.add(new Hamburguesa("Hamburguesa de queso","hola",2, 1500,"queso, carne ternera, pepinillo"));
 
+        ArrayList<Producto> combo1 = new ArrayList<Producto>();
+        combo1.add(carta1.get(2));
+        combo1.add(carta1.get(3));
+
+        carta1.add(new Combo("Placa Madre"
+                ,"Dos hamburguesas"
+                ,combo1
+        ));
+
 
         imprimeCarta(carta1);
 
